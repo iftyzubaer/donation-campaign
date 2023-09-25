e7a63586319cfcb849d275223e895e6724f9a8aa
iftyzubaer <iftyzubaer1304.com>
2023-09-25 23:56:53 +0600

    created fake json data

---
+++ src/Components/CategoryList/CategoryList.jsx
@@ -0,0 +1,9 @@
+const CategoryList = () => {
+    return (
+        <div>
+            <h2>12 donation card containers</h2>
+        </div>
+    );
+};
+
+export default CategoryList;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,11 @@
+import Banner from "../Banner/Banner";
+import CategoryList from "../CategoryList/CategoryList";
+
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            <CategoryList></CategoryList>
             <h2>This is home</h2>
         </div>
     );
