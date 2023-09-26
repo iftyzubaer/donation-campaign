ee6744f1d2d05c36b2397a0ec36d86dab48e0605
iftyzubaer <iftyzubaer1304.com>
2023-09-26 20:29:24 +0600

    created the banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,15 @@
 const Banner = () => {
     return (
         <div>
-            <h2>This is banner</h2>
+            <div className="hero min-h-[63vh]" style={{ backgroundImage: 'url("/images/Rectangle 4281.png")', backgroundRepeat: 'no-repeat', backgroundSize: 'contain' }}>
+                <div className="hero-overlay bg-white bg-opacity-90"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="">
+                        <h1 className="mb-5 text-5xl text-black font-bold">I Grow By Helping People In Need</h1>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
