0d2dfa54a059687fc38df9c4391b8790fd0da3f2
iftyzubaer <iftyzubaer1304.com>
2023-09-26 16:21:38 +0600

    styled donated page and show more button

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -5,6 +5,7 @@ const Donation = () => {
 
     const [donated, setDonated] = useState([])
     const [noFound, setNoFound] = useState("")
+    const [isShowMore, setIsShowMore] = useState(false)
     useEffect(() => {
         const donatedItems = JSON.parse(localStorage.getItem('donated'))
         
@@ -22,8 +23,16 @@ const Donation = () => {
                 noFound ? <p className="justify-center items-center flex h-[80vh]">{noFound}</p> :
                 <div>
                     <div className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-6">
-                        {donated.map(list => <DonatedList key={list.id} list={list}></DonatedList>)}
+                        {
+                            isShowMore ? donated.map(list => <DonatedList key={list.id} list={list}></DonatedList>) : donated.slice(0, 4).map(list => <DonatedList key={list.id} list={list}></DonatedList>)
+                        }
                     </div>
+                    <div className="text-center">
+                        {
+                            donated.length > 4 && <button onClick={() => setIsShowMore(!isShowMore)} className="px-7 py-4 bg-[#009444] rounded-lg text-base font-semibold text-white my-10">{isShowMore ? "Show less" : "Show More"}</button>
+                        }
+                    </div>
+                    
                 </div>
             }
         </div>
