0bf03975e0abf6f23f9b3891b67c0be79145a5d6
iftyzubaer <iftyzubaer1304.com>
2023-09-26 10:21:08 +0600

    created the donation cards and dynamic router for them

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 const List = ({list}) => {
-    const {image, category, title, textColor, categoryColor, cardColor} = list;
+    const {image, category, title, textColor, categoryColor, cardColor, id} = list;
     console.log(textColor, categoryColor);
     return (
         
-        <div>
+        <Link to={`/lists/${id}`}>
             <div className={`card bg-[${cardColor}]`}>
                 <figure><img src={image} alt="Shoes" /></figure>
                 <div className="card-body">
@@ -13,7 +14,7 @@ const List = ({list}) => {
                     <p className={`text-xl font-semibold text-[${textColor}]`}>{title}</p>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
